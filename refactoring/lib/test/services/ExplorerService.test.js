64f2b6cafde4b92ffa9cc9c1aeb355be7f1c4524
Luis <launchx04206@innovaccion.mx>
2022-04-30 18:53:56 -0500

    9. Creando test para obtener la cantidad de explorers que tienen node

--- refactoring/lib/test/services/ExplorerService.test.js
+++ refactoring/lib/test/services/ExplorerService.test.js
@@ -1,7 +1,11 @@
 const ExplorerService = require('./../../services/ExplorerService')
+const Reader = require("./../../utils/Reader")
 
 describe("Prueba de unidad para la clase Explorer Service", () => {
-    test('', () => { 
+    const explorer = Reader.readJsonFile("explorers.json")
 
-     })
+    test('Obtener la cantidad de explorers que tienen node en mission', () => { 
+        const explorersInNode = ExplorerService.filterByMission(explorer, "node")
+        expect(explorersInNode.length).toBe(9)
+     });
 })
