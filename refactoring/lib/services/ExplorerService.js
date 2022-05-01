08c7a1b0964f47d514c76fd39be253d12e66c056
Luis <launchx04206@innovaccion.mx>
2022-04-30 22:51:50 -0500

    12. Agregando metodos para obtener el username y la cantidad de explorers por mission

--- refactoring/lib/services/ExplorerService.js
+++ refactoring/lib/services/ExplorerService.js
@@ -3,6 +3,17 @@ class ExplorerService{
         const explorersInNode = explorers.filter((explorer) => explorer.mission === mission);
         return explorersInNode
     }
+
+    static getAmountOfExplorersByMission(explorers, mission){
+        const explorersByMission = explorers.filter((explorer) => explorer.mission == mission);
+        return explorersByMission
+    }
+
+    static getExplorersUsernamesByMission(explorers, mission){
+        const explorersInNode = explorers.filter((explorer) => explorer.mission === mission);
+        const usernamesInNode = explorersInNode.map((explorer) => explorer.githubUsername);
+        return usernamesInNode
+    }
 }
 
 module.exports = ExplorerService
