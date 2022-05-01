45c0a3fad5f0a6cd4d70d6a5aa876c0eab029e67
Luis <launchx04206@innovaccion.mx>
2022-04-30 22:52:40 -0500

    13. Llevando métodos al index

---
+++ refactoring/index.js
@@ -0,0 +1,8 @@
+const Reader = require ("./lib/utils/Reader")
+const ExplorerService = require("./lib/services/ExplorerService")
+
+const explorers = Reader.readJsonFile("explorers.json")
+//console.log(explorers)
+//console.log( ExplorerService.filterByMission(explorers, "node"))
+//console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node").length)
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
